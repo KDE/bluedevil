fa62851216320eaa39779bc4c5e248668f65a80c
Rafael Fernández López <ereslibre@kde.org>
2010-07-01 01:25:43 +0200

    Correctly update title if this adapter is the default one

--- src/kcmodule/bluedeviladapters.cpp
+++ src/kcmodule/bluedeviladapters.cpp
@@ -202,7 +202,11 @@ void AdapterSettings::readChanges()
     m_powered->setChecked(m_poweredOrig);
 
     m_discoverTimeLabel->setText(i18np("1 minute", "%1 minutes", m_discoverTime->value()));
-    setTitle(i18n("Adapter: %1 (%2)").arg(m_adapter->name()).arg(m_adapter->address()));
+    if (BlueDevil::Manager::self()->defaultAdapter() == m_adapter) {
+        setTitle(i18n("Default adapter: %1 (%2)").arg(m_adapter->name()).arg(m_adapter->address()));
+    } else {
+        setTitle(i18n("Adapter: %1 (%2)").arg(m_adapter->name()).arg(m_adapter->address()));
+    }
 
     blockSignals(false);
 
