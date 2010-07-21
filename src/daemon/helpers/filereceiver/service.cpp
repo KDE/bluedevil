66b242691da8c83268ee789e464c0aaf2803b1cc
Alex Fiestas <alex@eyeos.org>
2010-07-21 19:20:57 +0200

    Check if there is an adapter before use the object

--- src/daemon/helpers/filereceiver/service.cpp
+++ src/daemon/helpers/filereceiver/service.cpp
@@ -51,8 +51,13 @@ void Service::launchServer()
     if (m_server) {
         return;
     }
-    m_server = new OpenObex::Server(BlueDevil::Manager::self()->defaultAdapter()->address());
-    kDebug() << m_server;
+
+    if (BlueDevil::Manager::self()->defaultAdapter()) {
+        m_server = new OpenObex::Server(BlueDevil::Manager::self()->defaultAdapter()->address());
+        kDebug() << m_server;
+    } else{
+        kDebug() << "No adapters found";
+    }
 }
 
 void Service::stopServer()
