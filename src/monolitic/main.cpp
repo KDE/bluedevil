6692ff1324eab17a8c8613af1fb5a6fda2ba1d75
Alex Fiestas <alex@eyeos.org>
2010-07-21 20:00:18 +0200

    Monolitic must be a unique application

--- src/monolitic/main.cpp
+++ src/monolitic/main.cpp
@@ -18,7 +18,7 @@
 
 #include "monolitic.h"
 #include <KCmdLineArgs>
-#include <KApplication>
+#include <kuniqueapplication.h>
 #include <KAboutData>
 
 int main(int argc, char *argv[])
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 
     KCmdLineArgs::init(argc, argv, &aboutData);
 
-    KApplication app;
+    KUniqueApplication app;
     app.setQuitOnLastWindowClosed(false);
     new Monolitic;
 
