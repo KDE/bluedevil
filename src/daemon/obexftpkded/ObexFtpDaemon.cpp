e75cd097a8eeefd3419fef0c18c13d64afda4784
Alex Fiestas <alex@eyeos.org>
2010-12-12 23:38:46 +0100

    Increment the iterator, infinite loops are not good :/

--- src/daemon/obexftpkded/ObexFtpDaemon.cpp
+++ src/daemon/obexftpkded/ObexFtpDaemon.cpp
@@ -132,6 +132,7 @@ void ObexFtpDaemon::offlineMode()
             delete d->m_sessionMap[i.key()];
         }
         d->m_sessionMap.remove(i.key());
+        ++i;
     }
 
     delete d->m_manager;
