6f7ce9ea9b1151d843f6d5d58730ae3b712ca01b
Alex Fiestas <afiestas@kde.org>
2011-07-25 23:05:35 +0200

    First iteration on Network plugin, let's bridge NM and Bluetooth :)
    Thanks to all the work done by the NetworkManagement guys this is
    easy peacy, we just have to call configshell and it will do the rest.
    
    The plugin is not done though there is a bit of cleaning to do and also
    consider if we want Network to show up in systemtray/kio_bluetooth

---
+++ src/actionplugins/network/helper/networkhelper.cpp
@@ -0,0 +1,57 @@
+/*************************************************************************************
+ *  Copyright (C) 2010 by Alex Fiestas <alex@eyeos.org>                              *
+ *  Copyright (C) 2010 UFO Coders <info@ufocoders.com>                               *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#include "networkhelper.h"
+#include "../../actionplugin.h"
+
+#include <QApplication>
+#include <QDebug>
+
+#include <klocalizedstring.h>
+#include <kservicetypetrader.h>
+#include <ktoolinvocation.h>
+
+#include <bluedevil/bluedevil.h>
+
+using namespace BlueDevil;
+
+NetworkHelper::NetworkHelper(const KUrl& address) {
+    if (!BlueDevil::Manager::self()->defaultAdapter()) {
+        qDebug() << "No Adapters found";
+        qApp->exit();
+    }
+
+    Device *device = Manager::self()->defaultAdapter()->deviceForAddress(address.host().replace("-", ":"));
+
+    if(device->isPaired()) {
+        QString constraint("'00001103-0000-1000-8000-00805F9B34FB' in [X-BlueDevil-UUIDS]");
+
+        KPluginFactory *factory = KPluginLoader(
+            KServiceTypeTrader::self()->query("BlueDevil/ActionPlugin", constraint).first().data()->library()
+        ).factory();
+
+        ActionPlugin *plugin = factory->create<ActionPlugin>(this);
+        connect(plugin, SIGNAL(finished()), qApp, SLOT(quit()));
+
+        plugin->setDevice(device);
+        plugin->startAction();
+    } else {
+        KToolInvocation::kdeinitExec("bluedevil-wizard", QStringList() << address.url());
+    }
+}
