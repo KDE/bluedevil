912bd1e90c77cb50ec76e5fc3ae458aa656e9ccb
Alex Fiestas <afiestas@kde.org>
2011-07-31 02:23:54 +0200

    Renamed also the network-helper to network-dun-helper.

--- src/actionplugins/networkdun/helper/main.cpp
+++ src/actionplugins/networkdun/helper/main.cpp
@@ -18,7 +18,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
  ***************************************************************************/
 
-#include "networkhelper.h"
+#include "networkdunhelper.h"
 #include <KCmdLineArgs>
 #include <KApplication>
 #include <KAboutData>
--- src/actionplugins/networkdun/helper/networkdunhelper.cpp
+++ src/actionplugins/networkdun/helper/networkdunhelper.cpp
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
  *************************************************************************************/
 
-#include "networkhelper.h"
+#include "networkdunhelper.h"
 #include "../../actionplugin.h"
 
 #include <QApplication>
