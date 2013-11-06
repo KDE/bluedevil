e8098fede21ec19cbe96667e2acdbd05f6acdb5c
Àlex Fiestas <afiestas@kde.org>
2013-11-06 21:45:21 +0100

    Added types needed for obexd_file_transfer

---
+++ src/kio/obexftp/obexdtypes.h
@@ -0,0 +1,29 @@
+/*************************************************************************************
+ *  Copyright (C) 2013 by Alejandro Fiestas Fiestas <afiestas@kde.org>               *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#ifndef OBEXD_TYPES_H
+#define OBEXD_TYPES_H
+
+#include <QtCore/QList>
+#include <QtCore/QVariantMap>
+
+typedef QList<QVariantMap> QVariantMapList;
+
+Q_DECLARE_METATYPE(QVariantMapList);
+
+#endif //OBEXD_TYPES_H
