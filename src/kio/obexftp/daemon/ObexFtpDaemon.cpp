b526c50e81d3431839bff195405ee4ede0bb010e
Àlex Fiestas <afiestas@kde.org>
2013-11-06 18:57:30 +0100

    Rewrite obexftpdaemon, preapre for new way of doing obexftp.
    The idea goes as follow:
    
    The KDED will keep track only of sessions the rest will be done in the
    slave, for example this is how listDir will look like:
    
    -KIO asks KDED for a session
    -KDED returns an already existing one, or creates a new one
    -KIO then uses that sessionPath to call listDir
    
    All KDED operations will be Async, all KIO Sync.
    
    When a kio-slave needs a sessionm, will ask the KDED, in the case where

--- src/kio/obexftp/daemon/ObexFtpDaemon.cpp
+++ src/kio/obexftp/daemon/ObexFtpDaemon.cpp
@@ -1,31 +1,24 @@
-/***************************************************************************
- *   Copyright (C) 2010 Alejandro Fiestas Olivares <alex@eyeos.org>        *
- *   Copyright (C) 2010 UFO Coders <info@ufocoders.com>                    *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
- ***************************************************************************/
+/*************************************************************************************
+ *  Copyright (C) 2013 by Alejandro Fiestas Fiestas <afiestas@kde.org>               *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
 
 #include "ObexFtpDaemon.h"
 #include "version.h"
-#include "obexsession.h"
-#include "obexftp/listdirjob.h"
 
-#include "obexd_client.h"
-
-#include <QVariantMap>
 #include <QHash>
 
 #include <kdemacros.h>
@@ -35,16 +28,6 @@
 
 #include <bluedevil/bluedevilmanager.h>
 #include <bluedevil/bluedeviladapter.h>
-
-#define ENSURE_SESSION_CREATED(address) if (!d->m_sessionMap.contains(address)) { \
-        kDebug(dobex()) << "The address " << address << " doesn't has a session"; \
-        stablishConnection(address); \
-        return; \
-    } \
-    if (d->m_sessionMap[address]->status() == ObexSession::Connecting) { \
-        kDebug(dobex()) << "The session is waiting to be connected"; \
-        return; \
-    }
 
 using namespace BlueDevil;
 K_PLUGIN_FACTORY(ObexFtpFactory,
@@ -58,19 +41,15 @@ struct ObexFtpDaemon::Private
         Offline
     } m_status;
 
-    QHash <QString, ObexSession*> m_sessionMap;
-    QHash <QObject*, QString> m_sessionMapReverse;
-
-    org::bluez::obex::Client1 *m_client;
-
-    QEventLoop loop;
+    QHash <QString, QString> m_sessionMap;
 };
 
 ObexFtpDaemon::ObexFtpDaemon(QObject *parent, const QList<QVariant>&)
     : KDEDModule(parent)
     , d(new Private)
 {
-    d->m_client = 0;
+    d->m_status = Private::Offline;
+
     KAboutData aboutData(
         "obexftpdaemon",
         "bluedevil",
@@ -85,14 +64,12 @@ ObexFtpDaemon::ObexFtpDaemon(QObject *parent, const QList<QVariant>&)
         "http://www.afiestas.org");
 
     connect(Manager::self(), SIGNAL(usableAdapterChanged(Adapter*)),
-            this, SLOT(usableAdapterChanged(Adapter*)));
+            SLOT(usableAdapterChanged(Adapter*)));
 
-    d->m_status = Private::Offline;
+    //WARNING this blocks if org.bluez in system bus is dead
     if (Manager::self()->usableAdapter()) {
         onlineMode();
     }
-
-    qDBusRegisterMetaType<QVariantMapList>();
 }
 
 ObexFtpDaemon::~ObexFtpDaemon()
@@ -111,8 +88,6 @@ void ObexFtpDaemon::onlineMode()
         return;
     }
 
-    d->m_client = new org::bluez::obex::Client1("org.bluez.obex", "/org/bluez/obex", QDBusConnection::sessionBus(), this);
-
     d->m_status = Private::Online;
 }
 
@@ -124,301 +99,17 @@ void ObexFtpDaemon::offlineMode()
         return;
     }
 
-//     QHash<QString, ObexSession*>::const_iterator i = d->m_sessionMap.constBegin();
-//     while (i != d->m_sessionMap.constEnd()) {
-//         if (d->m_sessionMap.contains(i.key())) {
-//             d->m_sessionMap[i.key()]->Disconnect();
-//             d->m_sessionMap[i.key()]->Close();
-//             d->m_sessionMap[i.key()]->deleteLater();
-//         }
-//         d->m_sessionMap.remove(i.key());
-//         ++i;
-//     }
-//
-//     delete d->m_manager;
     d->m_status = Private::Offline;
 }
 
 void ObexFtpDaemon::usableAdapterChanged(Adapter *adapter)
 {
-    if (adapter) {
-        onlineMode();
-    } else {
+    if (!adapter) {
         offlineMode();
-    }
-}
-
-void ObexFtpDaemon::stablishConnection(QString dirtyAddress, const QDBusMessage& msg)
-{
-    QString address = cleanAddress(dirtyAddress);
-
-    kDebug(dobex()) << msg.service() << msg.path();
-    kDebug(dobex()) << "Address: " << address;
-    if (d->m_status == Private::Offline) {
-        kDebug(dobex()) << "We're offline, so do nothing";
-        QDBusMessage error = msg.createErrorReply("Offline", i18n("No Bluetooth device found"));
-        QDBusConnection::sessionBus().send(error);
         return;
     }
 
-    if (address.isEmpty()) {
-        kDebug(dobex()) << "Address is Empty";
-        QDBusMessage error = msg.createErrorReply("Invalid address", i18n("Invalid address"));
-        QDBusConnection::sessionBus().send(error);
-        return;
-    }
-
-
-    //We already have a session for that address
-    if (d->m_sessionMap.contains(address)) {
-        //But this session is waiting for being connected
-        if (d->m_sessionMap[address]->status() == ObexSession::Connecting) {
-            kDebug(dobex()) << "Session for this address is waiting for being connected";
-            d->m_sessionMap[address]->addMessage(msg);
-            return;
-        }
-
-        //At this point the session should be connected, if not we have done something wrong
-        Q_ASSERT(d->m_sessionMap[address]->status() == ObexSession::Connected);
-
-        kDebug(dobex()) << "We already have a session, so do nothing";
-        QDBusConnection::sessionBus().send(msg.createReply());
-        return;
-    }
-
-    kDebug(dobex()) << "Creating a new obexSession";
-
-    ObexSession *session = new ObexSession(address, d->m_client, msg, this);
-    connect(session, SIGNAL(destroyed(QObject*)), SLOT(sessionDestroyed(QObject*)));
-    d->m_sessionMap[address] = session;
-    d->m_sessionMapReverse[session] = address;
-}
-
-void ObexFtpDaemon::changeCurrentFolder(QString address, QString path)
-{
-    kDebug(dobex());
-//     d->m_sessionMap[address]->resetTimer();
-//     d->m_sessionMap[address]->ChangeCurrentFolderToRoot().waitForFinished();
-//
-//     QStringList list = path.split("/");
-//     Q_FOREACH(const QString &dir, list) {
-//         if (!dir.isEmpty() && dir != address) {
-//             kDebug(dobex()) << "Changing to: " << dir;
-//             QDBusPendingReply <void > a = d->m_sessionMap[address]->ChangeCurrentFolder(dir);
-//             a.waitForFinished();
-//             kDebug(dobex())  << "Change Error: " << a.error().message();
-//         } else {
-//             kDebug(dobex()) << "Skyping" << dir;
-//         }
-//     }
-}
-
-QVariantMapList ObexFtpDaemon::listDir(QString dirtyAddress, QString path, const QDBusMessage& msg)
-{
-    QString address = cleanAddress(dirtyAddress);
-    kDebug(dobex()) << "ROLFMAO !" << address;
-    if (!d->m_sessionMap.contains(address)) {
-        kDebug(dobex()) << "MA MEN";
-        return QVariantMapList();
-    }
-
-//     msg.setDelayedReply(true);
-//     ListDirJob *job = new ListDirJob(d->m_sessionMap[address], msg, this);
-//     job->start();
-
-QVariantMapList rolf;
-    QVariantMap map;
-    map["meh"] = "rolf";
-    rolf.append(map);
-    return rolf;
-//     QString address = cleanAddress(dirtyAddress);
-//     if (!d->m_sessionMap.contains(address)) {
-//         kDebug(dobex()) << "The address " << address << " doesn't has a session";
-//         stablishConnection(address);
-//         return QString();
-//     }
-//     if (d->m_sessionMap[address]->status() == ObexSession::Connecting) {
-//         kDebug(dobex()) << "The session is waiting to be connected";
-//         return QString();
-//     }
-//
-//     address.replace("-", ":");
-//     changeCurrentFolder(address, path);
-//
-//     d->m_sessionMap[address]->resetTimer();
-//     QDBusPendingReply<QString> reply = d->m_sessionMap[address]->RetrieveFolderListing();
-//     reply.waitForFinished();
-//     if (reply.isError()) {
-//         kDebug(dobex()) << reply.error().message();
-//         kDebug(dobex()) << reply.error().name();
-//         return QString();
-//     }
-//
-//     QString ret = reply.value();
-//
-//     kDebug(dobex()) << ret;
-//
-//     return ret;
-}
-
-void ObexFtpDaemon::copyRemoteFile(QString dirtyAddress, QString fileName, QString destPath)
-{
-    kDebug(dobex()) << destPath;
-//     QString address = cleanAddress(dirtyAddress);
-//     ENSURE_SESSION_CREATED(address);
-//
-//     KUrl url = KUrl(fileName);
-//     changeCurrentFolder(address, url.directory());
-//     kDebug(dobex()) << d->m_sessionMap[address]->GetCurrentPath().value();
-//     kDebug(dobex()) << url.fileName();
-//     d->m_sessionMap[address]->resetTimer();
-//     d->m_sessionMap[address]->CopyRemoteFile(url.fileName(), destPath);
-}
-
-void ObexFtpDaemon::sendFile(QString dirtyAddress, QString localPath, QString destPath)
-{
-    QString address = cleanAddress(dirtyAddress);
-
-//     kDebug(dobex());
-//     ENSURE_SESSION_CREATED(address);
-//     changeCurrentFolder(address, destPath);
-//
-//     d->m_sessionMap[address]->resetTimer();
-//     d->m_sessionMap[address]->SendFile(localPath);
-}
-
-void ObexFtpDaemon::createFolder(QString dirtyAddress, QString path)
-{
-    kDebug(dobex());
-//     QString address = cleanAddress(dirtyAddress);
-//     ENSURE_SESSION_CREATED(address);
-//
-//     KUrl url(path);
-//     changeCurrentFolder(address, url.directory());
-//
-//     d->m_sessionMap[address]->resetTimer();
-//     d->m_sessionMap[address]->CreateFolder(url.fileName()).waitForFinished();
-}
-
-void ObexFtpDaemon::deleteRemoteFile(QString dirtyAddress, QString path)
-{
-    kDebug(dobex());
-//     QString address = cleanAddress(dirtyAddress);
-//     ENSURE_SESSION_CREATED(address);
-//
-//     KUrl url(path);
-//     changeCurrentFolder(address, url.directory());
-//
-//     d->m_sessionMap[address]->resetTimer();
-//     d->m_sessionMap[address]->DeleteRemoteFile(url.fileName()).waitForFinished();;
-}
-
-bool ObexFtpDaemon::isBusy(QString dirtyAddress)
-{
-    return false;
-//     kDebug(dobex());
-//     QString address = cleanAddress(dirtyAddress);
-//     if (!d->m_sessionMap.contains(address)) {
-//         kDebug(dobex()) << "The address " << address << " doesn't has a session";
-//         stablishConnection(address);
-//         return true;//Fake the busy state, so stablishConneciton can work
-//     }
-//     if (d->m_sessionMap[address]->status() == ObexSession::Connecting) {
-//         kDebug(dobex()) << "The session is waiting to be connected";
-//         return true;
-//     }
-//
-//     d->m_sessionMap[address]->resetTimer();
-//     return d->m_sessionMap[address]->IsBusy().value();
-}
-
-void ObexFtpDaemon::Cancel(QString dirtyAddress)
-{
-    QString address = cleanAddress(dirtyAddress);
-//     ENSURE_SESSION_CREATED(address)
-//
-//     d->m_sessionMap[address]->resetTimer();
-//     d->m_sessionMap[address]->Cancel();
-}
-
-
-void ObexFtpDaemon::SessionConnected(QDBusObjectPath path)
-{
-    kDebug(dobex()) << "SessionConnected!" << path.path();
-/*
-    QString address = getAddressFromSession(path.path());
-
-    if (address.isEmpty() || !d->m_sessionMap.contains(address)) {
-        kDebug(dobex()) << "This seasson is from another process";
-        return;
-    }
-
-    d->m_sessionMap[address]->setStatus(ObexSession::Connected);
-
-    connect(d->m_sessionMap[address], SIGNAL(sessionTimeout()), this, SLOT(sessionDisconnected()));
-    connect(d->m_sessionMap[address], SIGNAL(Closed()), this, SLOT(sessionDisconnected()));
-    connect(d->m_sessionMap[address], SIGNAL(Disconnected()), this, SLOT(sessionDisconnected()));
-    connect(d->m_sessionMap[address], SIGNAL(sessionTimeout()), this, SIGNAL(Cancelled()));
-    connect(d->m_sessionMap[address], SIGNAL(Closed()), this, SIGNAL(Cancelled()));
-    connect(d->m_sessionMap[address], SIGNAL(Disconnected()), this, SIGNAL(Cancelled()));
-    connect(d->m_sessionMap[address], SIGNAL(Cancelled()), this, SIGNAL(Cancelled()));
-    connect(d->m_sessionMap[address], SIGNAL(TransferCompleted()), this, SIGNAL(transferCompleted()));
-    connect(d->m_sessionMap[address], SIGNAL(TransferProgress(qulonglong)), this, SIGNAL(transferProgress(qulonglong)));
-    connect(d->m_sessionMap[address], SIGNAL(ErrorOccurred(QString,QString)), this, SIGNAL(errorOccurred(QString,QString)));
-
-    emit sessionConnected(address);*/
-}
-
-void ObexFtpDaemon::SessionClosed(QDBusObjectPath path)
-{
-    kDebug(dobex());
-//     QHashIterator<QString, ObexSession*> i(d->m_sessionMap);
-//     while (i.hasNext()) {
-//         i.next();
-//         if (i.value()->path() == path.path()) {
-//             kDebug(dobex()) << "Removing : " << i.key();
-//             emit sessionClosed(i.key());
-//
-//             i.value()->deleteLater();
-//
-//             d->m_sessionMap.remove(i.key());
-//             return;
-//         }
-//     }
-//
-//     kDebug(dobex()) << "Attempt to remove a nto existing session";
-}
-
-void ObexFtpDaemon::sessionDisconnected()
-{
-    kDebug(dobex()) << "Session disconnected";
-//     ObexSession* session =  static_cast <ObexSession*>(sender());
-//     kDebug(dobex()) << session->path();
-//     kDebug(dobex()) << session->status();
-//
-//     d->m_sessionMap.remove(d->m_sessionMap.key(session));
-//     delete session;
-}
-
-void ObexFtpDaemon::sessionDestroyed(QObject* obj)
-{
-    kDebug(dobex()) << obj;
-    QString address = d->m_sessionMapReverse.take(obj);
-    d->m_sessionMap.remove(address);
-}
-
-QString ObexFtpDaemon::getAddressFromSession(QString path)
-{
-    kDebug(dobex()) << path;
-//     QStringMap info = d->m_manager->GetSessionInfo(QDBusObjectPath(path)).value();
-//     return info["BluetoothTargetAddress"];
-    return QString();
-}
-
-QString ObexFtpDaemon::cleanAddress(QString& dirtyAddress) const
-{
-    dirtyAddress.replace("-", ":");
-    return dirtyAddress.toLower();
+    onlineMode();
 }
 
 extern int dobex() { static int s_area = KDebug::registerArea("ObexDaemon", false); return s_area; }
--- src/kio/obexftp/daemon/ObexFtpDaemon.h
+++ src/kio/obexftp/daemon/ObexFtpDaemon.h
@@ -1,23 +1,20 @@
-/***************************************************************************
- *   Copyright (C) 2010 Alejandro Fiestas Olivares <alex@eyeos.org>        *
- *   Copyright (C) 2010 Eduardo Robles Elvira <edulix@gmail.com>           *
- *   Copyright (C) 2010 UFO Coders <info@ufocoders.com>                    *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
- ***************************************************************************/
+/*************************************************************************************
+ *  Copyright (C) 2013 by Alejandro Fiestas Fiestas <afiestas@kde.org>               *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
 
 #ifndef OBEXFTPDAEMON_H
 #define OBEXFTPDAEMON_H
@@ -27,8 +24,6 @@
 
 class QDBusMessage;
 class QDBusPendingCallWatcher;
-typedef QList<QVariantMap> QVariantMapList;
-Q_DECLARE_METATYPE(QVariantMapList);
 
 namespace BlueDevil {
     class Adapter;
@@ -39,14 +34,19 @@ class KDE_EXPORT ObexFtpDaemon
     : public KDEDModule
 {
     Q_OBJECT
-    Q_CLASSINFO("D-Bus Interface", "org.kde.ObexFtp")
+    Q_CLASSINFO("D-Bus Interface", "org.kde.obexftp")
 
 public:
     /**
-     * Stablish basics connections with libbluedevil signals and calls online if interfaces are availables
+     * Connects to usableAdapterChanged
      */
     ObexFtpDaemon(QObject *parent, const QList<QVariant>&);
     virtual ~ObexFtpDaemon();
+
+// public Q_SLOTS:
+
+private Q_SLOTS:
+    void usableAdapterChanged(Adapter* adapter);
 
 private:
     /**
@@ -61,58 +61,9 @@ private:
      */
     void offlineMode();
 
-    void changeCurrentFolder(QString address, QString path);
-
-    QString getAddressFromSession(QString path);
-
-    QString cleanAddress(QString& dirtyAddress) const;
-private:
     struct Private;
     Private *d;
-
-private Q_SLOTS:
-    /**
-     * Called when the default adapter changes, re-initialize the kded with the new
-     * default adapter
-     */
-    void usableAdapterChanged(Adapter *adapter);
-
-    void SessionConnected(QDBusObjectPath path);
-
-    void SessionClosed(QDBusObjectPath path);
-
-    void sessionDisconnected();
-
-    void sessionDestroyed(QObject*);
-
-//Dbus interface
-public Q_SLOTS:
-    /**
-     * Stablish a connection to the given address.
-     * When the session is connected, the "sessionConnected" signal will be emitted
-     */
-    Q_SCRIPTABLE void stablishConnection(QString dirtyAddress, const QDBusMessage &msg);
-
-    Q_SCRIPTABLE QVariantMapList listDir(QString address, QString path, const QDBusMessage &msg);
-
-    Q_SCRIPTABLE void copyRemoteFile(QString address, QString fileName, QString destPath);
-
-    Q_SCRIPTABLE void sendFile(QString address, QString localPath, QString destPath);
-
-    Q_SCRIPTABLE void createFolder(QString address, QString path);
-
-    Q_SCRIPTABLE void deleteRemoteFile(QString address, QString path);
-
-    Q_SCRIPTABLE bool isBusy(QString address);
-    Q_SCRIPTABLE void Cancel(QString address);
-
-Q_SIGNALS:
-    Q_SCRIPTABLE void sessionConnected(QString address);
-    Q_SCRIPTABLE void sessionClosed(QString address);
-    Q_SCRIPTABLE void Cancelled();
-    Q_SCRIPTABLE void transferProgress(qulonglong);
-    Q_SCRIPTABLE void transferCompleted();
-    Q_SCRIPTABLE void errorOccurred(QString,QString);
 };
+
 extern int dobex();
 #endif /*OBEXFTPDAEMON_H*/
