3416fab07bac6c04e14a341507356cdf0270491e
Àlex Fiestas <afiestas@kde.org>
2013-11-06 18:22:04 +0100

    Remove obextest, not needed anymore

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp | grep -v '/obextest/'` -o $podir/bluedevil.pot
+$XGETTEXT `find . -name \*.cpp` -o $podir/bluedevil.pot
