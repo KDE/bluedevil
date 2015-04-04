25b43374519e189c3c5c0f4ce24ff343da392055
David Rosca <nowrep@gmail.com>
2015-04-04 09:13:38 +0200

    Add new Bluetooth applet
    REVIEW: 123164

---
+++ src/applet/plugin/devicesproxymodel.h
@@ -0,0 +1,49 @@
+/*
+    Copyright 2014 David Rosca <nowrep@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DEVICESPROXYMODEL_H
+#define DEVICESPROXYMODEL_H
+
+#include <BluezQt/DevicesModel>
+#include <QSortFilterProxyModel>
+
+class DevicesProxyModel : public QSortFilterProxyModel
+{
+    Q_OBJECT
+
+public:
+    enum AdditionalRoles {
+        SectionRole = BluezQt::DevicesModel::LastRole + 10,
+        DeviceFullNameRole = BluezQt::DevicesModel::LastRole + 11,
+    };
+
+    DevicesProxyModel(QObject *parent = Q_NULLPTR);
+
+    QHash<int, QByteArray> roleNames() const Q_DECL_OVERRIDE;
+    QVariant data(const QModelIndex &index, int role) const Q_DECL_OVERRIDE;
+    bool lessThan(const QModelIndex &left, const QModelIndex &right) const Q_DECL_OVERRIDE;
+
+    Q_INVOKABLE QString adapterHciString(const QString &ubi) const;
+
+private:
+    bool duplicateIndexAddress(const QModelIndex &idx) const;
+};
+
+#endif // DEVICESPROXYMODEL_H
---
+++ src/applet/plugin/launchapp.h
@@ -0,0 +1,39 @@
+/*
+    Copyright 2014 David Rosca <nowrep@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef LAUNCHAPP_H
+#define LAUNCHAPP_H
+
+#include <QObject>
+#include <QStringList>
+
+class LaunchApp : public QObject
+{
+    Q_OBJECT
+
+public:
+    LaunchApp(QObject *parent = Q_NULLPTR);
+
+public Q_SLOTS:
+    bool runCommand(const QString &exe, const QStringList &args = QStringList());
+    bool runUrl(const QString &url, const QString &mimeType);
+};
+
+#endif // LAUNCHAPP_H
