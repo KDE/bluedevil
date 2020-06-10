KDE/bluedevil

commit c810d1f3c8f2c7d8f513f4aa4b9b87261382ca1c

[applet] Improve tooltip text for single connected device use case
Summary:
Also fixes the bug that was making the popup be too wide in this case because we're no
longer using a newline in the text (will fix that for the multi-device case in another
patch later).

Test Plan:
Before: {F8247608}
After: {F8247607}

Reviewers: #vdg, #plasma, drosca, nicolasfella

Reviewed By: nicolasfella

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D28997

src/applet/package/contents/ui/logic.js
@@ -51,7 +51,9 @@ function updateStatus()
         } else {
             text = i18n("Bluetooth is offline");
         }
-    } else if (connectedDevices.length) {
+    } else if (connectedDevices.length == 1) {
+        text = i18n("%1 connected", device.name);
+    } else if (connectedDevices.length > 1) {
         text = i18ncp("Number of connected devices", "%1 connected device", "%1 connected devices", connectedDevices.length);
         for (var i = 0; i < connectedDevices.length; ++i) {
             var device = connectedDevices[i];

commit 29a535b1edfbd46b77b04ae43cd279729f0e6b82

[applet] Fix tooltip showing wrong name for connected device
This was introduced with c810d1f3c8f2c7d8f513f4aa4b9b87261382ca1c
and is my fault, sorry. `device` is undefined in the single device use
case; we need to do `connectedDevices[0]` instead to make sure we get
the right one.

BUG: 422691
FIXED-IN: 5.19.1

src/applet/package/contents/ui/logic.js
@@ -52,7 +52,7 @@ function updateStatus()
             text = i18n("Bluetooth is offline");
         }
     } else if (connectedDevices.length == 1) {
-        text = i18n("%1 connected", device.name);
+        text = i18n("%1 connected", connectedDevices[0].name);
     } else if (connectedDevices.length > 1) {
         text = i18ncp("Number of connected devices", "%1 connected device", "%1 connected devices", connectedDevices.length);
         for (var i = 0; i < connectedDevices.length; ++i) {