df8d7b2327d882e986f4d712cd43a5c0c3632144
David Redondo <kde@david-redondo.de>
2020-09-09 21:52:18 +0200

    Convert copyright statements to SPDX expressions

--- src/kio/obexftp/kioobexftp.h
+++ src/kio/obexftp/kioobexftp.h
@@ -1,25 +1,12 @@
-/***************************************************************************
- *   This file is part of the KDE project                                  *
- *                                                                         *
- *   Copyright (C) 2010 Alejandro Fiestas Olivares <alex@ufocoders.com>    *
- *   Copyright (C) 2010 UFO Coders <info@ufocoders.com>                    *
- *   Copyright (C) 2014-2015 David Rosca <nowrep@gmail.com>                *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
- ***************************************************************************/
+/*
+ *   This file is part of the KDE project
+ *
+ *   SPDX-FileCopyrightText: 2010 Alejandro Fiestas Olivares <alex@ufocoders.com>
+ *   SPDX-FileCopyrightText: 2010 UFO Coders <info@ufocoders.com>
+ *   SPDX-FileCopyrightText: 2014-2015 David Rosca <nowrep@gmail.com>
+ *
+ *   SPDX-License-Identifier: GPL-2.0-or-later
+ */
 
 #ifndef KIO_OBEXFTP_H
 #define KIO_OBEXFTP_H
