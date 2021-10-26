577d1bf4d69c537f1c2a8cc25d3fae19c3334a4c
Laurent Montel <montel@kde.org>
2021-10-26 07:48:11 +0200

    GIT_SILENT: add missing overide

--- src/kded/devicemonitor.h
+++ src/kded/devicemonitor.h
@@ -24,7 +24,7 @@ class DeviceMonitor : public QObject
 
 public:
     explicit DeviceMonitor(BlueDevilDaemon *daemon);
-    ~DeviceMonitor();
+    ~DeviceMonitor() override;
 
     void saveState();
 
