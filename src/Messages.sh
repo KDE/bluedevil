a86c4be3f4acfd0e99f07a3b5f8fecef7f06b8bb
Ismael Asensio <isma.af@gmail.com>
2026-07-30 22:27:28 +0200

    i18n: Fix dialog translation for Forget Device
    The 'Forget Device' dialog has been moved into a qml component, which
    also means a different translation domain (`bluedevil.pot` insetad of
    `kcm_bluetooth.pot`).
    
    Fix the dialog translation by:
    - extracting the qml messages into the catalog
    - explicitly reference the catalog (`i18nd()`)
    - avoid duplicating messages on several catalogs
    
    (cherry picked from commit ab73c5a472f9856d93ac3aa94a9c39855dc1ed8e)

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/bluedevil.pot
+$XGETTEXT `find . \( -name \*.cpp -o -name \*.qml \) ! -path "*applet*" ! -path "*kcm*"` -o $podir/bluedevil.pot
